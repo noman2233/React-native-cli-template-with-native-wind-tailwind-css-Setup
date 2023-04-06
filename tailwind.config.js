1ad7220a09b31af8a492ccce132eab7b5f8d53f2
Noman Sanaullah <99878827+noman2233@users.noreply.github.com>
2023-04-06 11:08:36 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require('nativewind/tailwind/native');
+module.exports = {
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './screens/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [nativewind()],
+};
